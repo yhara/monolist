168b1d4485bbe5689d3a5f245b8bc94ce34cbd95
Yutaka HARA <yutaka.hara+github@gmail.com>
2014-04-15 15:39:33 +0900

    fix test

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class TopControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
+    assert_redirected_to controller: 'folders', action: 'show', id: 1
   end
 
 end
